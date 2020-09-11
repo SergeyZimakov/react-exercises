798126364fbea90c289f1346e51875cbee04abcc
ameedf <ameed.software@gmail.com>
2020-09-11 12:47:44 +0300

    Update Password1.js
    Remark inside

--- src/components/mySolutions/Password1.js
+++ src/components/mySolutions/Password1.js
@@ -5,6 +5,7 @@ class Password1 extends React.Component {
 
 
     render() {
+//// secret should be provided as a property
         const secret = Math.floor(Math.random() * Math.floor(9));
         const guess = Math.floor(Math.random() * Math.floor(9));
         let color = (secret === guess) ? 'green' : 'red';
